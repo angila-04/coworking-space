8bb37858abdc58e79bfb59a248e2b6e3f4156012
Angila Maria <angilamaria2004@gmail.com>
2025-12-20 12:29:15 +0530

    Restructure React frontend and ignore node_modules

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AdminLayout from "./admin/AdminLayout";
+import AdminDashboard from "./admin/pages/AdminDashboard";
+import ManageSpaces from "./admin/pages/ManageSpaces";
+import ManageBookings from "./admin/pages/ManageBookings";
+import ManageUsers from "./admin/pages/ManageUsers";
+import ServiceRequests from "./admin/pages/ServiceRequests";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="spaces" element={<ManageSpaces />} />
+          <Route path="bookings" element={<ManageBookings />} />
+          <Route path="users" element={<ManageUsers />} />
+          <Route path="services" element={<ServiceRequests />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
