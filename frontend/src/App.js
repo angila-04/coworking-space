a2de5a98392110c329365b7a530c3749c2eb2608
linm123456 <linmary1404@gmail.com>
2026-01-08 09:48:55 +0530

    Added Service Provider layout and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,17 @@ import ManageBookings from "./admin/pages/ManageBookings";
 import ManageUsers from "./admin/pages/ManageUsers";
 import ServiceRequests from "./admin/pages/ServiceRequests";
 
+
+/* Service Provider imports  */
+import ServiceProviderLayout from "./layouts/ServiceProviderLayout";
+import Dashboard from "./pages/serviceProvider/Dashboard";
+import PaymentsBilling from "./pages/serviceProvider/PaymentsBilling";
+import ReportsAnalytics from "./pages/serviceProvider/ReportsAnalytics";
+import ActivityLogs from "./pages/serviceProvider/ActivityLogs";
+import AssignedTasks from "./pages/serviceProvider/AssignedTasks";
+import TaskDetails from "./pages/serviceProvider/TaskDetails";
+import Notifications from "./pages/serviceProvider/Notifications";
+
 function App() {
   return (
     <BrowserRouter>
@@ -18,6 +29,19 @@ function App() {
           <Route path="users" element={<ManageUsers />} />
           <Route path="services" element={<ServiceRequests />} />
         </Route>
+
+
+        {/* 🔹 SERVICE PROVIDER MODULE (NEW) */}
+        <Route path="/service-provider" element={<ServiceProviderLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="tasks" element={<AssignedTasks />} />
+          <Route path="tasks/:id" element={<TaskDetails />} />
+          <Route path="payments" element={<PaymentsBilling />} />
+          <Route path="reports" element={<ReportsAnalytics />} />
+          <Route path="activity-logs" element={<ActivityLogs />} />
+          <Route path="notifications" element={<Notifications />} />
+        </Route>
+        
       </Routes>
     </BrowserRouter>
   );
